526bf7dfeecc862890e16b4935d653d12846157f
Hafiz Mobeen Ahmed <114430057+hafizmobeen23@users.noreply.github.com>
2024-06-23 16:07:26 +0500

    Add files via upload

---
+++ StudentService.java
@@ -0,0 +1,11 @@
+public class StudentService {
+    private final StudentManager studentManager;
+
+    public StudentService(StudentManager studentManager) {
+        this.studentManager = studentManager;
+    }
+
+    public void performStudentOperations() {
+        // Use studentManager to perform operations
+    }
+}
